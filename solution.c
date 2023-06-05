97c38635c2fe02f01f9918815c72d082af1644b6
AdityaSrivastava3107 <kallakallz123@gmail.com>
2023-06-05 16:36:13 +0530

    Second solution because the first one was not working

--- solution.c
+++ solution.c
@@ -1,8 +1,8 @@
-// Solution using CRC-16 ALGORITHM
+#include <stdio.h>
 #include <stdint.h>
 
 #define MAX_PACKET_DATA_LENGTH (50)
-
+// Given data packet
 typedef struct data_packet_t {
     uint8_t id;
     uint8_t data_length;
@@ -10,37 +10,50 @@ typedef struct data_packet_t {
     uint16_t crc;
 } data_packet_t;
 
-// CRC algorithm implementation
-// function to calculate CRC
-uint16_t calculate_crc(const uint8_t* data, uint8_t length) {
-    uint16_t crc = 0xFFFF; //Initialization of crc to 0xFFFF
+// CRC 16 Algorithm
+uint16_t calculate_crc(const uint8_t *data, uint8_t length) {
+    uint16_t crc = 0xFFFF;
+    uint16_t polynomial = 0x1021;
 
-    for (uint8_t i = 0; i < length; i++) {
-        crc ^= (uint16_t)data[i] << 8;
-
-        for (uint8_t j = 0; j < 8; j++) {
-            if (crc & 0x8000) {
-                crc = (crc << 1) ^ 0x1021;  // Polynomial: x^16 + x^12 + x^5 + 1
-            } else {
-                crc <<= 1;
-            }
+    for (uint8_t i = 0; i < length; ++i) {
+        crc ^= (uint16_t)(data[i] << 8);
+        for (uint8_t j = 0; j < 8; ++j) {
+            crc = (crc & 0x8000) ? ((crc << 1) ^ polynomial) : (crc << 1);
         }
     }
 
     return crc;
 }
 
-// Method to check if a data packet is corrupted
-int is_packet_corrupted(const data_packet_t* packet) {
-    // Calculate the CRC of the received packet (excluding the crc field)
-    uint16_t received_crc = packet->crc;
-    packet->crc = 0;  // Set the crc field to zero before calculating CRC
-    uint16_t calculated_crc = calculate_crc((const uint8_t*)packet, sizeof(data_packet_t));
+// Check if the data packet is corrupted
+int is_packet_corrupted(const data_packet_t *packet) {
+    uint16_t calculated_crc = calculate_crc(packet->data, packet->data_length);
 
-    // Compare the calculated CRC with the received CRC
-    if (received_crc == calculated_crc) {
-        return 0;  // Packet is not corrupted
+    if (calculated_crc == packet->crc) {
+        return 0; // Not corrupted
     } else {
-        return 1;  // Packet is corrupted
+        return 1; // Corrupted
     }
+}
+
+int main() {
+    data_packet_t packet;
+    packet.id = 1;
+    packet.data_length = 5;
+    packet.data[0] = 'H';
+    packet.data[1] = 'e';
+    packet.data[2] = 'l';
+    packet.data[3] = 'l';
+    packet.data[4] = 'o';
+    packet.crc = calculate_crc(packet.data, packet.data_length);
+
+    int is_corrupted = is_packet_corrupted(&packet);
+
+    if (is_corrupted) {
+        printf("Packet is corrupted.\n");
+    } else {
+        printf("Packet is not corrupted.\n");
+    }
+
+    return 0;
 }
