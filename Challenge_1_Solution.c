436d21f02b939275246b878f454080d6d64c2a35
vishalsingh8 <91191282+vishalsingh8@users.noreply.github.com>
2023-06-10 11:23:11 +0530

    Add files via upload

---
+++ Challenge_1_Solution.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stdbool.h>
+#define MAX_PACKET_DATA_LENGTH (50)
+
+typedef struct data_packet_t {
+    uint8_t id;
+    uint8_t data_length;
+    uint8_t data[MAX_PACKET_DATA_LENGTH];
+    uint16_t crc;
+} data_packet_t;
+
+uint16_t calculateCRC(const uint8_t *data, size_t length) {
+    uint16_t crc = 0xFFFF;  
+    
+    for (size_t i = 0; i < length; i++) {
+        crc ^= (uint16_t)data[i];  
+        
+        for (int j = 0; j < 8; j++) {
+            if (crc & 0x0001) {
+                crc >>= 1;
+                crc ^= 0xA001;  
+            } else {
+                crc >>= 1;
+            }
+        }
+    }
+    
+    return crc;
+}
+
+bool isPacketCorrupted(const data_packet_t *packet) {
+    uint16_t crc = calculateCRC((const uint8_t *)packet, sizeof(data_packet_t) - sizeof(uint16_t));
+    
+    return (crc != packet->crc);
+}
+
+int main() {
+    data_packet_t packet = {
+        .id = 0x01,
+        .data_length = 4,
+        .data = {0x12, 0x34, 0x56, 0x78},
+        .crc = 0x0000
+    };
+
+    packet.crc = calculateCRC((const uint8_t *)&packet, sizeof(data_packet_t) - sizeof(uint16_t));
+
+    packet.data[2] = 0xAA;
+
+    if (isPacketCorrupted(&packet)) {
+        printf("Data Packet is corrupted.\n");
+    } else {
+        printf("Data Packet is not corrupted.\n");
+    }
+
+    return 0;
+}
